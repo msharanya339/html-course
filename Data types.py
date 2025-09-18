ac0437f7cad7dabd9171f30f24be89434304bceb
msharanya339 <msharanya339@gmail.com>
2025-09-18 22:36:40 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,21 @@
+name = "john" 
+age = 12
+weight=30.7
+isStudent = True
+
+print("Value:",name,"Data type:",type(name))
+print("Value:",age,"Data Type:",type(age))
+
+age = float(age)
+print("Converted to float:", age)
+
+weight = int(weight)
+print("Convered to int:", weight)
+
+num ='100'
+num = int(num)
+print("Value:",num, "Dta Type: ",type(num))
+
+
+
+
---
+++ operators.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter first value:"))
+print("Input value 1:",num1)
+
+num2 = int(input("Enter second value:"))
+print("operations -->")
+print("Addition:", num1 = num2)
+print("Subtraction:", num1 - num2)
+print("Floor Divition:", num1 // num2)
+print("Modulus:", num1 % num2)
+
+print(5**2)
+
