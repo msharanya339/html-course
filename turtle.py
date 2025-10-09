b1271ba281ea291cd9ddf7a63b4718a65220c460
msharanya339 <msharanya339@gmail.com>
2025-10-09 22:13:09 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,11 @@
+my_turtle = (1,"HELLO ,3,5")
+print(my_turtle)
+
+student_marks = ("jhon",6[20,40,40,30],(10,11,12))
+#turtle_marjs = 8
+print(student_marks[2])
+print(student_marks[2][0])
+print(student_marks[3],[1])
+
+
+
